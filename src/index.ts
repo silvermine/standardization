b49268c3bf1e5f4cc0457d3a8713ebc9e65c9d0d
Paul Bredenberg <paulbredenberg@gmail.com>
2020-08-28 09:34:38 -0400

    fix: add more inclusive glob to exclude prerelease tags

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { autoChangelogCommand } from './auto-changelog-config';
 
 // Shared variables used by various configurations.
-const LATEST_VALID_TAG_COMMAND = 'git describe --tags --abbrev=0 --exclude "*rc*" $(git rev-list --tags --max-count=1)';
+const LATEST_VALID_TAG_COMMAND = 'git describe --tags --abbrev=0 --exclude "v*.*.*-*" $(git rev-list --tags --max-count=1)';
 
 const CHANGELOG_INFILE = 'CHANGELOG.md';
 
