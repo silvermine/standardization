c5d0f3cc393031ca4e53b7576a220778ed9a1927
Ethan Smith <ethan@onebytegone.com>
2022-02-25 15:19:29 -0500

    feat: add script to simplify the package release process (#27)

--- README.md
+++ README.md
@@ -115,6 +115,138 @@ grunt.registerTask('standards', [ 'markdownlint' ]);
 
 TODO: fill in details and examples here.
 
+
+### Release Process
+
+#### Configuration
+
+   1. Ensure that the project's `package.json` file has a
+      [`repository.url`][package-json-repo-url] field with the URL to the canonical repo
+      for the project in its git hosting solution, e.g.
+      <https://github.com/silvermine/event-emitter.git> for the
+      [@silvermine/event-emitter](https://github.com/silvermine/event-emitter) project.
+      * This is necessary because conventional-changelog needs to know the URL to the git
+         hosting solution so that it can make links to "compare URLs" in the CHANGELOG
+   1. Add the following NPM scripts to the project's `package.json` file:
+
+      ```json
+      "release:preview": "node ./node_modules/@silvermine/standardization/scripts/release.js preview",
+      "release:prep-changelog": "node ./node_modules/@silvermine/standardization/scripts/release.js prep-changelog",
+      "release:finalize": "node ./node_modules/@silvermine/standardization/scripts/release.js finalize"
+      ```
+
+   1. (Optional) If the project is using an issue tracking system other than what the git
+      hosting solution provides (e.g. the code is hosted on GitHub but uses Azure DevOps
+      for issue tracking), add this config to the project's `package.json`:
+
+      ```json
+      "silvermine-standardization": {
+         "changelog": {
+            "issueUrlFormat": "https://issuetracker.example.com/tickets/{{id}}"
+         }
+      }
+      ```
+
+[package-json-repo-url]: https://docs.npmjs.com/cli/v8/configuring-npm/package-json#repository
+
+#### Release a New Package Version
+
+At a high-level, the process for releasing a new version of a package is:
+
+   1. Generate the new changelog entries (See [Prepare the
+      Changelog](#prepare-the-changelog)
+   1. Submit the changelog updates through the standard code review process
+   1. Update the version number in `package.json` and create the version tag (See [Perform
+      the Version Bump](#perform-the-version-bump))
+
+##### Prepare the Changelog
+
+   1. Checkout and update the branch that is to be released
+      * For example, if you are working off of `master`:
+
+         ```bash
+         git fetch --all
+         git checkout master
+         git reset origin/master --hard
+         git log -n 5
+         ```
+
+   1. Install the NPM dependencies and ensure the tests pass:
+
+      ```bash
+      npm ci
+      npm run standards && npm test
+      ```
+
+   1. Run `npm release:prep-changelog`. You should now be on a branch named
+      `changelog-v${NEW_VERSION}` containing the automatically generated changelog
+      additions.
+      * If you receive the message "There were no changelog entries generated" and this is
+        expected, please proceed to [Perform the Version Bump](#perform-the-version-bump).
+      * If the changelog needs to be edited, please make the needed adjustments and amend
+        your edits to the "chore: update changelog" commit.
+      * When preparing the changelog for a final version, the release candidate changelog
+        entries will be removed and the changelog will be regenerated for the release.
+        Please ensure that any edits made to the release candidate changelog entries are
+        reapplied to the final changelog.
+   1. Push the branch to the correct remote repo and open a pull request.
+
+##### Perform the Version Bump
+
+   1. Once the changelog has been merged, checkout and update the branch that is to be
+      released. The last commit should be the merge commit for the updates to the
+      changelog.
+   1. Run `npm release:finalize`
+   1. Preview the changes and push the branch and `v${NEW_VERSION}` tag to the correct
+      remote repo
+   1. If the version should be published and this is not handled by a CI/CD pipeline, run
+      `npm publish` to publish the package
+
+##### Special Cases
+
+In most cases, `npm release:preview`, `npm release:prep-changelog`, and `npm
+release:finalize` will be run without any additional options. However, there are a few
+cases when you may need to supply extra options.
+
+###### First Release
+
+When a package is first created, the package.json typically says the version is v0.1.0.
+If that's the version you want to generate the changelog for and publish to NPM, there's a
+problem. The release script will want to bump the package to v0.2.0 or v0.1.1. As such, a
+version of 0.1.0 has to be specified using the `--version` option. For example:
+
+```bash
+npm release:preview -- --version 0.1.0
+npm release:prep-changelog -- --version 0.1.0
+npm release:finalize -- --version 0.1.0
+```
+
+###### Releasing v1.0.0
+
+When a package is &lt;v1.0.0, breaking changes will not bump the package to the next major
+version. As such, a package's version will stay &lt;v1.0.0 until you tell the release
+script to publish v1.0.0. This can be done using the `--version` option. For example:
+
+```bash
+npm release:preview -- --version 1.0.0
+npm release:prep-changelog -- --version 1.0.0
+npm release:finalize -- --version 1.0.0
+```
+
+###### Prerelease Version (e.g. Alpha, Beta, Release Candidate)
+
+If you would like to create a prerelease of the next version, you can use the
+`--prerelease` option to specify the prerelease type. For example, let's say your package
+is currently at v1.0.2 and v1.1.0 will be the next version. However, you'd like to create
+a v1.1.0-rc.0 before creating the final v1.1.0. To do this, you can pass a `--prerelease
+rc` option (Values like `alpha` and `beta` also work). For example:
+
+```bash
+npm release:preview -- --prerelease rc
+npm release:prep-changelog -- --prerelease rc
+npm release:finalize -- --prerelease rc
+```
+
 ## License
 
 This software is released under the MIT license. See [the license file](LICENSE) for more
---
+++ scripts/release.js
@@ -0,0 +1,311 @@
+/* eslint no-console: [ "error", { allow: [ "info", "warn", "error" ] } ] */
+'use strict';
+
+const commander = require('commander'),
+      chalk = require('chalk'),
+      conventionalChangelog = require('conventional-changelog'),
+      conventionalRecommendedBump = require('conventional-recommended-bump'),
+      semver = require('semver'),
+      fs = require('fs'),
+      path = require('path'),
+      childProcess = require('child_process'),
+      util = require('util'),
+      gitSemverTags = util.promisify(require('git-semver-tags')),
+      getRecommendedBump = util.promisify(conventionalRecommendedBump),
+      execFile = util.promisify(childProcess.execFile);
+
+const CHANGELOG_CONFIG = {
+   filename: 'CHANGELOG.md',
+   releaseEntryPattern: /(^#+ \[?[0-9]+\.[0-9]+\.[0-9]+|<a name=)/m,
+   header: '# Changelog\n\nAll notable changes to this project will be documented in this file.\n'
+      + 'See [our coding standards][commit-messages] for commit guidelines.\n\n',
+   footer: '[commit-messages]: https://github.com/silvermine/silvermine-info/blob/master/commit-history.md#commit-messages',
+};
+
+function isPrereleaseVersion(version) {
+   return !!semver.prerelease(version);
+}
+
+async function calculateNextVersion(currentVersion, prereleaseIdentifier) {
+   const recommendedBump = await getRecommendedBump({
+      preset: {
+         name: 'conventionalcommits',
+         preMajor: semver.lt(currentVersion, '1.0.0'),
+      },
+   });
+
+   let releaseType = recommendedBump.releaseType;
+
+   if (prereleaseIdentifier) {
+      if (isPrereleaseVersion(currentVersion)) {
+         // TODO: We might need to consider adjusting this to support bumping to the next
+         // version (e.g. a patch prerelease contains a new feature)
+         // See: https://github.com/conventional-changelog/standard-version/blob/6c75ed0b14f/lib/lifecycles/bump.js#L46-L50
+         releaseType = 'prerelease';
+      } else {
+         releaseType = `pre${releaseType}`; // e.g. premajor, preminor, prepatch
+      }
+   }
+
+   return semver.inc(currentVersion, releaseType, prereleaseIdentifier);
+}
+
+/**
+ * Returns the last, based on semver ordering, final release tag (i.e. v1.0.0, not
+ * v1.0.0-rc.0) found in the git log for the current branch.
+ */
+async function getLastFinalReleaseTag() {
+   const semverTags = await gitSemverTags({
+      skipUnstable: true,
+   });
+
+   return semverTags.length ? semver.rsort(semverTags)[0] : undefined;
+}
+
+function createChangelogAdditions(opts) {
+   return new Promise((resolve, reject) => {
+      let content = '';
+
+      // https://github.com/conventional-changelog/conventional-changelog/tree/master/packages/conventional-changelog-core#options
+      const changelogOptions = {
+         preset: {
+            name: 'conventionalcommits',
+            issueUrlFormat: opts.issueUrlFormat,
+         },
+         skipUnstable: opts.ignorePrereleaseTags,
+      };
+
+      // https://github.com/conventional-changelog/conventional-changelog/tree/master/packages/conventional-changelog-writer#context
+      const context = { version: opts.version };
+
+      // https://github.com/conventional-changelog/conventional-changelog/tree/master/packages/conventional-changelog-writer#options
+      const writerOptions = {
+         generateOn: (commit) => {
+            if (opts.ignorePrereleaseTags && isPrereleaseVersion(commit.version)) {
+               return false;
+            }
+
+            return semver.valid(commit.version);
+         },
+      };
+
+      const changelogStream = conventionalChangelog(changelogOptions, context, undefined, undefined, writerOptions);
+
+      changelogStream.on('error', (err) => {
+         return reject(err);
+      });
+
+      changelogStream.on('data', (buffer) => {
+         content += buffer.toString();
+      });
+
+      changelogStream.on('end', () => {
+         // In the event the there are no entries added to the changelog (i.e. it's just a
+         // changelog header), we don't want to add anything to the changelog
+         if (content.trim().split('\n').length === 1) {
+            resolve(undefined);
+         }
+
+         resolve(content);
+      });
+   });
+}
+
+async function addEntriesToChangelog(newEntries) {
+   const existingChangelog = await (fs.promises.readFile(CHANGELOG_CONFIG.filename, 'utf-8').catch(() => { return ''; })),
+         existingEntriesStartIndex = existingChangelog.search(CHANGELOG_CONFIG.releaseEntryPattern);
+
+   let existingChangelogEntries = CHANGELOG_CONFIG.footer; // NOTE: Only added when creating a new changelog
+
+   if (existingEntriesStartIndex !== -1) {
+      existingChangelogEntries = existingChangelog.substring(existingEntriesStartIndex);
+   }
+
+   const newChangelog = `${CHANGELOG_CONFIG.header}${newEntries}\n${existingChangelogEntries}`;
+
+   await fs.promises.writeFile(CHANGELOG_CONFIG.filename, newChangelog.trimEnd() + '\n');
+}
+
+async function createNewBranch(name) {
+   await execFile('git', [ 'checkout', '-b', name ]);
+}
+
+async function resetChangelogToTag(tag) {
+   await execFile('git', [ 'checkout', tag, '--', CHANGELOG_CONFIG.filename ]);
+}
+
+async function commitChangelog(version) {
+   await execFile('git', [ 'add', CHANGELOG_CONFIG.filename ]);
+   await execFile('git', [ 'commit', '-m', `chore: update changelog for v${version}` ]);
+}
+
+async function getCurrentBranchName() {
+   const output = await execFile('git', [ 'rev-parse', '--abbrev-ref', 'HEAD' ]);
+
+   return output.stdout.trim();
+}
+
+async function doesRepositoryHaveUncommittedChanges() {
+   const output = await execFile('git', [
+      'status',
+      '--porcelain',
+   ]);
+
+   return output.stdout !== '';
+}
+
+const main = async (argv) => {
+   function quitWithError(msg) {
+      console.error(chalk.red.bold(`${msg}\n`));
+      process.exit(1); // eslint-disable-line no-process-exit
+   }
+
+   if (await doesRepositoryHaveUncommittedChanges()) {
+      quitWithError('The repository has uncommitted changes. Please ensure the repository is clean before running this script');
+   }
+
+   const program = new commander.Command(),
+         packageJSON = JSON.parse(await fs.promises.readFile(path.join(process.cwd(), 'package.json'))),
+         currentVersion = packageJSON.version,
+         changelogConfig = packageJSON['silvermine-standardization'] ? packageJSON['silvermine-standardization'].changelog : {},
+         prereleaseOption = new commander.Option('--prerelease <type>', 'The type of prerelease, e.g. alpha, beta, rc'),
+         versionOption = new commander.Option('--version <version>', 'The version to use instead of the auto-calculated version');
+
+   async function getNextVersion(options) {
+      if (options.version) {
+         const providedVersion = semver.clean(options.version);
+
+         if (!providedVersion) {
+            quitWithError(`Invalid version: ${options.version}`);
+         }
+
+         if (options.prerelease) {
+            quitWithError(
+               'The --prerelease flag cannot be used with --version.'
+               + ` Please specify the desired prerelease in the version, e.g. ${providedVersion}-${options.prerelease}.0`
+            );
+         }
+
+         return providedVersion;
+      }
+
+      return await calculateNextVersion(currentVersion, options.prerelease);
+   }
+
+   program
+      .command('preview')
+      .description('Preview the version and changelog for the next release')
+      .addOption(prereleaseOption)
+      .addOption(versionOption)
+      .action(async (options) => {
+         const targetVersion = await getNextVersion(options);
+
+         console.info(chalk.yellow(`Will bump ${packageJSON.name} from v${currentVersion} to v${targetVersion}`));
+         console.info(chalk.whiteBright('\nAutogenerated changelog:\n'));
+         console.info(await createChangelogAdditions({
+            version: targetVersion,
+            ignorePrereleaseTags: !isPrereleaseVersion(targetVersion),
+            issueUrlFormat: changelogConfig.issueUrlFormat,
+         }));
+      });
+
+   program
+      .command('prep-changelog')
+      .description('Generates the changelog for the next release')
+      .addOption(prereleaseOption)
+      .addOption(versionOption)
+      .action(async (options) => {
+         const targetVersion = await getNextVersion(options),
+               isFinalVersion = !isPrereleaseVersion(targetVersion),
+               changelogBranch = `changelog-v${targetVersion}`;
+
+         console.info(`Generating changelog for ${packageJSON.name}@${targetVersion}...`);
+
+         const changelogAdditions = await createChangelogAdditions({
+            version: targetVersion,
+            ignorePrereleaseTags: isFinalVersion,
+            issueUrlFormat: changelogConfig.issueUrlFormat,
+         });
+
+         if (!changelogAdditions) {
+            console.warn(chalk.yellow(
+               'There were no changelog entries generated. Aborting changelog preparation.'
+               + ' If this is expected (e.g. only documentation or internal changes were made),'
+               + ' this version can be finalized without an update to the changelog.'
+            ));
+            return;
+         }
+
+         await createNewBranch(changelogBranch);
+
+         if (isFinalVersion && semver.diff(currentVersion, targetVersion) === 'prerelease') {
+            const lastFinalReleaseTag = await getLastFinalReleaseTag();
+
+            if (lastFinalReleaseTag) {
+               console.info(chalk.yellow(`Resetting changelog to ${lastFinalReleaseTag} to remove prerelease entries...`));
+               await resetChangelogToTag(lastFinalReleaseTag);
+            } else {
+               console.info(chalk.red(
+                  'Could not find a tag for a non-prerelease version. Not resetting the changelog to remove prerelease entries.'
+               ));
+            }
+         }
+
+         await addEntriesToChangelog(changelogAdditions);
+
+         await commitChangelog(targetVersion);
+
+         console.info(
+            chalk.whiteBright('The changelog has been updated. Please do the following:\n\n')
+            + `   ${chalk.gray('1.')} git show ${chalk.gray('# and review the changes')}\n`
+            + `   ${chalk.gray('2.')} git push $REMOTE_NAME ${changelogBranch}\n`
+            + `   ${chalk.gray('3.')} Create a merge request\n`
+         );
+      });
+
+   program
+      .command('finalize')
+      .description('Performs the version bump for the release')
+      .addOption(prereleaseOption)
+      .addOption(versionOption)
+      .action(async (options) => {
+         const targetVersion = await getNextVersion(options),
+               currentBranch = await getCurrentBranchName();
+
+         console.info(`Bumping ${packageJSON.name} from v${currentVersion} to v${targetVersion}...`);
+
+         await execFile('npm', [
+            'version',
+            // `--allow-same-version` makes it so it's possible to have an initial release
+            // where the current version equals the target version
+            '--allow-same-version',
+            targetVersion,
+            '-m',
+            `chore: version bump: v${targetVersion}`,
+         ]);
+
+         console.info(
+            chalk.whiteBright('The package version has been bumped. Please do the following:\n\n')
+            + `   ${chalk.gray('1.')} git show ${chalk.gray('# and review the changes')}\n`
+            + `   ${chalk.gray('2.')} git push $REMOTE_NAME ${currentBranch} v${targetVersion}\n`
+            + `   ${chalk.gray('3.')} If the package needs to be manually published, run "npm publish"\n`
+         );
+      });
+
+   program.action(() => { program.outputHelp(); });
+   program.exitOverride();
+
+   try {
+      await program.parseAsync(argv);
+   } catch(err) {
+      if (err instanceof commander.CommanderError) {
+         program.outputHelp();
+      } else {
+         console.error(chalk.red.bold(`ERROR: ${err}`) + `\n${err.stack}`);
+      }
+
+      process.exit(1); // eslint-disable-line no-process-exit
+   }
+};
+
+main(process.argv);
